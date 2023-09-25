cec82b4bc9796890f471378dd7adfb7e6eace7c7
Érica Lima <ericahvlima@gmail.com>
2023-09-25 16:30:05 +0100

    novo2

--- src/app/activity.ts
+++ src/app/activity.ts
@@ -1,4 +1,5 @@
 export interface Activity1 {
     id: number;
     name: string;
+    des: string;
   }
--- src/app/mock-acitivies.ts
+++ src/app/mock-acitivies.ts
@@ -1,13 +1,13 @@
 import { Activity1 } from './activity';
 
 export const ACTIVITY: Activity1 [] = [
-  { id: 12, name: 'Activity 1' },
-  { id: 13, name: 'Activity 2' },
-  { id: 14, name: 'Activity 3' },
-  { id: 15, name: 'Activity 4' },
-  { id: 16, name: 'Activity 5' },
-  { id: 17, name: 'Activity 6' },
-  { id: 18, name: 'Activity 7' },
-  { id: 19, name: 'Activity 8' },
-  { id: 20, name: 'Activity 9' }
+  { id: 12, name: 'Activity 1' , des: 'x'},
+  { id: 13, name: 'Activity 2', des: 'x' },
+  { id: 14, name: 'Activity 3' , des: 'x'},
+  { id: 15, name: 'Activity 4', des: 'x' },
+  { id: 16, name: 'Activity 5' , des: 'x'},
+  { id: 17, name: 'Activity 6', des: 'x' },
+  { id: 18, name: 'Activity 7', des: 'x' },
+  { id: 19, name: 'Activity 8' , des: 'x'},
+  { id: 20, name: 'Activity 9' , des: 'x'}
 ];  
