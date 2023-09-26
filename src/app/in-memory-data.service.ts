bc3251a7f91d228f897b6e2318ed3c29f6b525c3
Érica Lima <ericahvlima@gmail.com>
2023-09-26 10:14:37 +0100

    com algumas atividades

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,11 +8,11 @@ import { Activity } from './activity';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const activities = [
-      { id: 12, name: 'Activity 1', description: 'eewwwe1' },
-      { id: 13, name: 'Activity 2' },
-      { id: 14, name: 'Activity 3' },
-      { id: 15, name: 'Activity 4' },
-      { id: 16, name: 'Activity 5' },
+      { id: 12, name: 'Activity 1', description: 'Dress with your eyes closed' },
+      { id: 13, name: 'Activity 2', description: 'Eat different types of food' },
+      { id: 14, name: 'Activity 3', description: 'Check the time in a mirror' },
+      { id: 15, name: 'Activity 4', description: 'Switch the computer mouse aside '},
+      { id: 16, name: 'Activity 5', description: 'Brush your teeth using your left hand (or your right, if you are left-handed)' },
       { id: 17, name: 'Activity 6' },
       { id: 18, name: 'Activity 7' },
       { id: 19, name: 'Activity 8' },
